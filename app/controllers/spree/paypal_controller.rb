1a8e1c9912ec2b50cd553f49dc219758fca431ce
Taku Nakajima <tnakajima@degica.com>
2017-11-16 18:54:03 +0900

    Minimum confirm

--- app/controllers/spree/paypal_controller.rb
+++ app/controllers/spree/paypal_controller.rb
@@ -33,11 +33,12 @@ module Spree
       begin
         pp_response = provider.set_express_checkout(pp_request)
         if pp_response.success?
+          order = current_order || raise(ActiveRecord::RecordNotFound)
           payment_source = Spree::PaypalExpressCheckout.create({
                                                          :state => 'processing',
                                                          :token => pp_response.token,
+                                                         :order_number => order.number
                                                        }, :without_protection => true)
-          order = current_order || raise(ActiveRecord::RecordNotFound)
           order.payments.create!({
                                    :source => payment_source,
                                    :amount => order.total,
@@ -60,21 +61,20 @@ module Spree
     end
 
     def confirm
-      order = current_order || raise(ActiveRecord::RecordNotFound)
-      order.payments.create!({
-        :source => Spree::PaypalExpressCheckout.create({
-          :token => params[:token],
-          :payer_id => params[:PayerID]
-        }, :without_protection => true),
-        :amount => order.total,
-        :payment_method => payment_method
-      }, :without_protection => true)
-      order.next
-      if order.complete?
-        flash.notice = t(:order_processed_successfully)
-        flash[:commerce_tracking] = "nothing special"
-        session[:order_id] = nil
-        redirect_to order_path(order, :token => order.token)
+      if payment_source = Spree::PaypalExpressCheckout.where(token: params[:token]).first
+        payment_source.update_attributes({
+                                          payer_id: params[:PayerID],
+                                          state: 'complete'
+                                        }, without_protection: true)
+        if order = Spree::Order.by_number(payment_source.order_number).first
+          order.payment.purchase!
+          flash.notice = t(:order_processed_successfully)
+          flash[:commerce_tracking] = "nothing special"
+          session[:order_id] = nil
+          redirect_to order_path(order, :token => order.token)
+        else
+          redirect_to checkout_state_path(order.state)
+        end
       else
         redirect_to checkout_state_path(order.state)
       end
